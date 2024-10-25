39ce575dafc1aa3662af00f0f9a601a159d8780b
Andrew Yastangacal <andrewkyle945@gmail.com>
2024-10-25 16:28:47 -0400

    Changes based on feedback

--- src/test/java/SerializationTest.java
+++ src/test/java/SerializationTest.java
@@ -9,6 +9,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.function.Function;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -23,25 +24,25 @@ public class SerializationTest {
      */
     @Test
     public void testExport() throws IOException {
-        Graph<String, DefaultEdge> g = new DefaultDirectedGraph<>(DefaultEdge.class);
+        Graph<String, DefaultEdge> exportGraph1 = new DefaultDirectedGraph<>(DefaultEdge.class);
 
         // Sample graph based on the simple open, close, error FSM for files
         String open = "Open";
         String close = "Close";
         String error = "Error";
-        g.addVertex(open);
-        g.addVertex(close);
-        g.addVertex(error);
+        exportGraph1.addVertex(open);
+        exportGraph1.addVertex(close);
+        exportGraph1.addVertex(error);
 
-        g.addEdge(open,close);
-        g.addEdge(close,close);
-        g.addEdge(open,error);
+        exportGraph1.addEdge(open,close);
+        exportGraph1.addEdge(close,close);
+        exportGraph1.addEdge(open,error);
 
 
         // Exports the graph into DOT format
-        DOTExporter<String, DefaultEdge> de = new DOTExporter<>(Vertex -> Vertex);
+        DOTExporter<String, DefaultEdge> exporter = new DOTExporter<>(Vertex -> Vertex);
         File output = new File("src/test/resources/test-output/output1");
-        de.exportGraph(g, output);
+        exporter.exportGraph(exportGraph1, output);
 
         long exitCode = Files.mismatch(Paths.get(output.getAbsolutePath()),Paths.get("src/test/resources/expected1"));
 
@@ -58,27 +59,30 @@ public class SerializationTest {
     public void testImportGraphFromFile() throws IOException {
 
         // Checking deserialization of "import1" graph/FSM
-        DefaultDirectedGraph<String, DefaultEdge> G = new DefaultDirectedGraph<>(DefaultEdge.class);
-        DOTImporter<String, DefaultEdge> di = new DOTImporter<>();
-        di.setVertexFactory(v -> v);
+        DefaultDirectedGraph<String, DefaultEdge> testGraph1 = new DefaultDirectedGraph<>(DefaultEdge.class);
+        DOTImporter<String, DefaultEdge> importer = new DOTImporter<>();
+        // JGraphT by default doesn't format DOT with vertex names.
+        // Use identity function to replace vertex indices with names in output.
+        importer.setVertexFactory(Function.identity());
 
         try (FileReader reader = new FileReader("src/test/resources/import1")) {
-            di.importGraph(G, reader);
+            importer.importGraph(testGraph1, reader);
         }
 
         String expectedToString = "([Open, Close, Error], [(Open,Close), (Close,Close), (Open,Error)])";
-        assertEquals(expectedToString, G.toString(), "Graph String representation test 1 failed");
+        assertEquals(expectedToString.replaceAll("\\s",""), testGraph1.toString().replaceAll("\\s",""), "Graph String representation test 1 failed");
 
         // Checking deserialization of "import2" graph/FSM
-        G = new DefaultDirectedGraph<>(DefaultEdge.class);
+        testGraph1 = new DefaultDirectedGraph<>(DefaultEdge.class);
         try (FileReader reader = new FileReader("src/test/resources/import2")) {
-            di.importGraph(G, reader);
+            importer.importGraph(testGraph1, reader);
         }
 
 
 
         String expectedToString2 = "([Not Studying, Studying, Playing], [(Not Studying,Studying), (Not Studying,Playing), " +
                 "(Studying,Playing), (Playing,Not Studying), (Playing,Studying)])";
-        assertEquals(expectedToString2, G.toString(), "Graph String representation test 2 failed");
+        //Ignoring whitespaces to check equality
+        assertEquals(expectedToString2.replaceAll("\\s",""), testGraph1.toString().replaceAll("\\s",""), "Graph String representation test 2 failed");
     }
 }
