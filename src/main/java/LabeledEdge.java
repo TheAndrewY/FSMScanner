81eb11d3e63bef8628fc330c12d976c884add20d
Andrew <andrewkyle945@gmail.com>
2024-12-10 12:57:29 -0500

    Merged main into branch and changed code based on feedback

--- src/main/java/LabeledEdge.java
+++ src/main/java/LabeledEdge.java
@@ -1,31 +1,29 @@
-import org.jgrapht.graph.DefaultEdge;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-/**
- * LabeledEdge class extending DefaultEdge to give
- * edges/transitions in FSMs/Graphs labels.
- */
+import java.util.List;
+import org.jgrapht.graph.DefaultEdge;
+
+/** LabeledEdge class extending DefaultEdge to give edges/transitions in FSMs/Graphs labels. */
 public class LabeledEdge extends DefaultEdge {
-    private final ArrayList<String> labels;
+  private final List<String> labels;
 
-    /**
-     * Constructs a LabeledEdge with the specified label(s)
-     *
-     * @param labels the label(s) associated with this edge
-     */
-    public LabeledEdge(String... labels) {
-        this(new ArrayList<>(Arrays.asList(labels)));
-    }
+  /**
+   * Constructs a LabeledEdge with the specified label(s)
+   *
+   * @param labels the label(s) associated with this edge
+   */
+  public LabeledEdge(String... labels) {
+    this(new ArrayList<>(Arrays.asList(labels)));
+  }
 
-    /**
-     * Constructs a LabeledEdge with the specific list of labels
-     *
-     * @param labels A list containing multiple labels that represent the edge
-     */
-    public LabeledEdge(ArrayList<String> labels) {
-        this.labels = labels;
-    }
+  /**
+   * Constructs a LabeledEdge with the specific list of labels
+   *
+   * @param labels A list containing multiple labels that represent the edge
+   */
+  public LabeledEdge(List<String> labels) {
+    this.labels = labels;
+  }
 
   /**
    * Returns the source vertex of this edge.
@@ -45,32 +43,29 @@ public class LabeledEdge extends DefaultEdge {
     return (String) super.getTarget();
   }
 
-    /**
-     * Returns the list of labels of this edge
-     *
-     * @return the ArrayList of labels
-     */
-    public ArrayList<String> getLabels(){
-        return labels;
+  public List<String> getLabels() {
+    return labels;
+  }
+
+  /**
+   * Adds a label to the labels list
+   *
+   * @param label the label which is being added
+   */
+  public void addLabel(String label) {
+    labels.add(label);
+  }
+
+  /**
+   * String representation of this edge is just the label, or all labels is multiple are present.
+   *
+   * @return the label of this edge, or list of labels if there are multiple labels
+   */
+  @Override
+  public String toString() {
+    if (labels.size() == 1) {
+      return labels.get(0);
     }
-    /**
-     * Adds a label to the labels list
-     *
-     * @param label the label which is being added
-     */
-    public void addLabel(String label){
-        labels.add(label);
-    }
-    /**
-     * String representation of this edge is just the label, or all labels is multiple are present.
-     *
-     * @return the label of this edge, or list of labels if there are multiple labels
-     */
-    @Override
-    public String toString() {
-        if(labels.size()==1){
-            return labels.get(0);
-        }
-        return labels.toString();
-    }
+    return labels.toString();
+  }
 }
--- src/test/java/AlgoTest.java
+++ src/test/java/AlgoTest.java
@@ -14,76 +14,85 @@ import org.junit.jupiter.api.Test;
 /** Test Class specializing in Algorithm testing. */
 public class AlgoTest {
 
-    /**
-     * Tests input of supplied FSM and output of all error-inducing subsequences for non-accumulation typestate FSM 3.3: C-style pointers.
-     *
-     * @throws IOException when provided 'algo-test1' file is not found or error in reading file.
-     */
-    @Test
-    public void algoTest1() throws IOException {
-        MyUtils utils = new MyUtils();
-        DefaultDirectedGraph<String, LabeledEdge> algoGraph1 = utils.dotToFSM("src/test/resources/algo-test1");
-        Set<GraphPath<String, LabeledEdge>> errSubseqs = utils.allErrPaths(algoGraph1,"valid");
-        //If the found number of error-inducing subsequences is not equal, immediately fail testcase
-        assertEquals(errSubseqs.size(), 6, "# of error-inducing subsequences does not match");
+  /**
+   * Tests input of supplied FSM and output of all error-inducing subsequences for non-accumulation
+   * typestate FSM 3.3: C-style pointers.
+   *
+   * @throws IOException when provided 'algo-test1' file is not found or error in reading file.
+   */
+  @Test
+  public void algoTest1() throws IOException {
+    MyUtils utils = new MyUtils();
+    DefaultDirectedGraph<String, LabeledEdge> algoGraph1 =
+        utils.dotToFSM("src/test/resources/algo-test1");
+    Set<GraphPath<String, LabeledEdge>> errSubseqs = utils.allErrPaths(algoGraph1, "valid");
+    // If the found number of error-inducing subsequences is not equal, immediately fail testcase
+    assertEquals(errSubseqs.size(), 6, "# of error-inducing subsequences does not match");
 
-        // Set of predetermined (manually found) expected results
-        // String representation of all subsequences/paths
-        HashSet<String> expectedSet = new HashSet<>();
-        expectedSet.add("[assign valid, free, free]");
-        expectedSet.add("[free, free]");
-        expectedSet.add("[assign null, free]");
-        expectedSet.add("[assign valid, assign null, free]");
-        expectedSet.add("[free, assign null, free]");
-        expectedSet.add("[assign valid, free, assign null, free]");
-        for (GraphPath<String, LabeledEdge> x : errSubseqs) {
-            assertTrue(expectedSet.contains(x.toString()), "error-inducing subsequence mismatch");
-        }
+    // Set of predetermined (manually found) expected results
+    // String representation of all subsequences/paths
+    HashSet<String> expectedSet = new HashSet<>();
+    expectedSet.add("[assign valid, free, free]");
+    expectedSet.add("[free, free]");
+    expectedSet.add("[assign null, free]");
+    expectedSet.add("[assign valid, assign null, free]");
+    expectedSet.add("[free, assign null, free]");
+    expectedSet.add("[assign valid, free, assign null, free]");
+    for (GraphPath<String, LabeledEdge> x : errSubseqs) {
+      assertTrue(expectedSet.contains(x.toString()), "error-inducing subsequence mismatch");
     }
-    /**
-     * Tests the pathToGraph method in Utils specifically with "import3" FSM/Graph
-     *
-     * @throws IOException if an I/O error occurs during the reading of the DOT file.
-     */
+  }
 
-    @Test
-    public void pathToGraph1() throws IOException {
-        MyUtils utils = new MyUtils();
-        DefaultDirectedGraph<String, LabeledEdge> origGraph = utils.dotToFSM("src/test/resources/import3");
-        AllDirectedPaths<String,LabeledEdge> adp = new AllDirectedPaths<>(origGraph);
-        ArrayList<String> pathGraphs = new ArrayList<>();
-        for(GraphPath<String,LabeledEdge> x : adp.getAllPaths("Not Studying","Sleeping",true,null)){
-            pathGraphs.add(utils.pathToGraph(x).toString().replaceAll("\\s", ""));
-        }
-        ArrayList<String> validGraphs = new ArrayList<>();
-        validGraphs.add("([NotStudying,Studying,Playing,Sleeping],[Study=(NotStudying,Studying),Play=(Studying,Playing),Sleep=(Playing,Sleeping)])");
-        validGraphs.add("([NotStudying,Playing,Sleeping],[Play=(NotStudying,Playing),Sleep=(Playing,Sleeping)])");
-        validGraphs.add("([NotStudying,Sleeping],[Sleep=(NotStudying,Sleeping)])");
-        assertTrue(pathGraphs.containsAll(validGraphs));
+  /**
+   * Tests the pathToGraph method in Utils specifically with "import3" FSM/Graph
+   *
+   * @throws IOException if an I/O error occurs during the reading of the DOT file.
+   */
+  @Test
+  public void pathToGraph1() throws IOException {
+    MyUtils utils = new MyUtils();
+    DefaultDirectedGraph<String, LabeledEdge> origGraph =
+        utils.dotToFSM("src/test/resources/import3");
+    AllDirectedPaths<String, LabeledEdge> adp = new AllDirectedPaths<>(origGraph);
+    ArrayList<String> pathGraphs = new ArrayList<>();
+    for (GraphPath<String, LabeledEdge> x :
+        adp.getAllPaths("Not Studying", "Sleeping", true, null)) {
+      pathGraphs.add(utils.pathToGraph(x).toString().replaceAll("\\s", ""));
     }
-    /**
-     * Tests the generateSubpaths method on the Error-inducing subsequences generated from a DOT file FSM.
-     *
-     * @throws IOException if an I/O error occurs during the reading of the DOT file.
-     */
-    @Test
-    public void subpathTest1() throws IOException {
-        MyUtils utils = new MyUtils();
-        DefaultDirectedGraph<String, LabeledEdge> origGraph = utils.dotToFSM("src/test/resources/algo-test2");
-        List<String> expectedSubpaths = new ArrayList<>();
-        Set<String> generatedErrSubpathsString = new HashSet<>();
-        expectedSubpaths.add("[next(true)]");
-        expectedSubpaths.add("[next(false), [next(true), next(false)]]");
-        expectedSubpaths.add("[next(true), next(false)]");
-        expectedSubpaths.add("[next(true), next(false), [next(true), next(false)]]");
-        expectedSubpaths.add("[next(false)]");
-        expectedSubpaths.add("[[next(true), next(false)]]");
-        for(GraphPath<String,LabeledEdge> errPath : utils.allErrPaths(origGraph,"start")){
-            for(GraphPath<String,LabeledEdge> subErrPath : utils.generateSubpaths(errPath)){
-                generatedErrSubpathsString.add(subErrPath.toString());
-            }
-        }
-        assertTrue(expectedSubpaths.containsAll(generatedErrSubpathsString));
-        assertTrue(generatedErrSubpathsString.containsAll(expectedSubpaths));
+    ArrayList<String> validGraphs = new ArrayList<>();
+    validGraphs.add(
+        "([NotStudying,Studying,Playing,Sleeping],[Study=(NotStudying,Studying),Play=(Studying,Playing),Sleep=(Playing,Sleeping)])");
+    validGraphs.add(
+        "([NotStudying,Playing,Sleeping],[Play=(NotStudying,Playing),Sleep=(Playing,Sleeping)])");
+    validGraphs.add("([NotStudying,Sleeping],[Sleep=(NotStudying,Sleeping)])");
+    assertTrue(pathGraphs.containsAll(validGraphs));
+  }
+
+  /**
+   * Tests the generateSubpaths method on the Error-inducing subsequences generated from a DOT file
+   * FSM.
+   *
+   * @throws IOException if an I/O error occurs during the reading of the DOT file.
+   */
+  @Test
+  public void subpathTest1() throws IOException {
+    MyUtils utils = new MyUtils();
+    DefaultDirectedGraph<String, LabeledEdge> origGraph =
+        utils.dotToFSM("src/test/resources/algo-test2");
+    List<String> expectedSubpaths = new ArrayList<>();
+    Set<String> generatedErrSubpathsString = new HashSet<>();
+    expectedSubpaths.add("[next(true)]");
+    expectedSubpaths.add("[next(false), [next(true), next(false)]]");
+    expectedSubpaths.add("[next(true), next(false)]");
+    expectedSubpaths.add("[next(true), next(false), [next(true), next(false)]]");
+    expectedSubpaths.add("[next(false)]");
+    expectedSubpaths.add("[[next(true), next(false)]]");
+    for (GraphPath<String, LabeledEdge> errPath : utils.allErrPaths(origGraph, "start")) {
+      for (GraphPath<String, LabeledEdge> subErrPath : utils.generateSubpaths(errPath)) {
+        generatedErrSubpathsString.add(subErrPath.toString());
+      }
     }
+    assertTrue(expectedSubpaths.containsAll(generatedErrSubpathsString));
+    assertTrue(generatedErrSubpathsString.containsAll(expectedSubpaths));
+  }
 }
